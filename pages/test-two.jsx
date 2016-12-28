965980a19cfea6b85a489aaf94ba634505dd8936
Taylor Boren <taylor.boren@galvanize.com>
2016-12-28 12:27:41 -0700

    fixed bin

--- pages/test-two.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-require('newrelic');
-import TestTwo from './test-two.view';
-
-ReactDOM.render(<TestTwo {...window.__APP_INITIAL_STATE__} />,
-  document.getElementById('root')
-)
